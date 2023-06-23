044b5ffcfec877e27b6b229c1f8c011fedf30778
Cristina Padró <34605397+SilverArrow23@users.noreply.github.com>
2023-06-23 15:33:39 +0200

    aaa

--- src/components/paginas/Noticias.js
+++ src/components/paginas/Noticias.js
@@ -13,6 +13,45 @@ export default function Noticias() {
         <Container className='news-spec-box-container'>
           <Box className='news-spec-box'>
             <div className='news-spec-img-container-l'>
+              <img src='images/nunk.jpeg' alt='horario' className='news-spec-img' />
+            </div>
+          </Box>
+        </Container>
+        <Container className='news-spec-box-container'>
+          <Box className='news-spec-box-t'>
+            <div className='news-spec-text-container'>
+              <h1>Curso intensivo nunchakus y la forma de Xing Yi Quan</h1>
+            </div>
+          </Box>
+        </Container>
+      </div>
+      <div className='news-spec-container'>
+        <Container className='news-spec-box-container'>
+          <Box className='news-spec-box'>
+            <div className='news-spec-img-container-l'>
+              <img src='images/act.jpeg' alt='horario' className='news-spec-img' />
+            </div>
+          </Box>
+        </Container>
+        <Container className='news-spec-box-container'>
+          <Box className='news-spec-box-t'>
+            <div className='news-spec-text-container'>
+              <h1>CHIU CHI LING visita el centro SHAOLIN VALENCIA</h1>
+              <p>
+                  Hemos tenido el gran honor y placer de contar con la visita del Actor y Maestro de Artes Marciales Chiu Chi Ling que, en su periplo europeo, ha pasado unos días en la ciudad de Valencia, y en su apretada agenda, nos ha obsequiado con una amena tarde en Shaolin Valencia. <br/>
+                  Chiu Chi Ling es un reconocido actor que ha compartido pantalla con actores de la talla de Jackie Chan o Jet Li, y que los amantes de las películas de artes marciales recordamos en actuaciones inolvidables como La Serpiente a la Sombra del Aguila (1978) o más reciente en Kung Fu Sion (2004) o en tutoriales como Tiger and Crane Shaolin Kung Fu (1982), que se han convertido en grandes clásicos del cine de Artes Marciales. <br/>
+                  El Maestro Chiu Chi Ling es el máximo representante actual del estilo Hung Gar, que aprendió en el seno de su familia bajo la tutela directa de su padre, el Gran Maestro Chiu Kao.
+                  Amigo personal del maestro Zhu QiHui, director de Shaolin Valencia, con el que también ha compartido pantalla y coreografía en varias películas en China, ha hecho gala de su enorme vitalidad y condición física durante la vista a Shaolin Valencia, una clara muestra de la excelencia que supone una vida dedicada a la práctica de las Artes Marciales Chinas.<br/>
+                  Gracias Maestro Chiu Chi Ling!
+                  </p>
+            </div>
+          </Box>
+        </Container>
+      </div>
+      <div className='news-spec-container'>
+        <Container className='news-spec-box-container'>
+          <Box className='news-spec-box'>
+            <div className='news-spec-img-container-l'>
               <img src='images/Anyo_nuevo_chino.jpeg' alt='horario' className='news-spec-img' />
             </div>
           </Box>
